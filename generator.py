fb9846b497bd14ec61fc1c14408d7c807a3d1976
Claud Xiao <secmobi@gmail.com>
2013-05-08 10:38:21 +0800

    rewrited the generator and deleted unnecessary files

--- generator.py
+++ generator.py
@@ -1,39 +1,45 @@
-f = open('data.txt')
-line = f.readline()
+#!/usr/bin/env python
 
-default_initials = open("default.scad", "r")
-default_initials = default_initials.read()
+ROW_MAX = 4
+COL_MAX = 3
 
-template = open("template.scad", "r")
-template = template.read()
+SCAD_BEGIN = """include <Candal.scad>
 
-output = open('whistles1.scad', 'w');
-output.write(default_initials);
+w = 44;
+h = 28;
+"""
 
-row = 1;
-col = 1;
-filenr = 1;
-print "FILE NR1\n";
-print "================================ \n";
-while line:
-	initials = "l"+str(row)+str(col)+" = \""+line[0]+line[1]+"\";\n";
-	output.write(initials);
-	print initials;
-	col = col+1;
-	if (col == 4):
-		row = row + 1;
-		col = 1;
-		if (row == 5):
-			output.write(template);
-			output.close();
-			row = 1
-			filenr = filenr + 1;
-			output = open("whistles"+str(filenr)+".scad", 'w');
-			output.write(default_initials);
-			print "FILE NR"+str(filenr)+"\n";
-			print "================================ \n";
-			
-	line = f.readline()
-output.write(template);
-output.close();
-f.close()
+SCAD_TEMPLATE = """
+translate([%d*w, %d*h, 0]) {
+     import_stl("blank-whistle.stl");
+     translate([-9, -5.5, 20])scale([0.25, 0.25, 0.2]) {
+         translate([0, 0, 0]) color("red") Candal("%s", steps = 3, center = true);
+     }
+}
+"""
+
+fn = 1
+output = open('whistle%d.scad' % fn, 'w')
+output.write(SCAD_BEGIN)
+
+line = 0
+for name in open('data.txt', 'r').readlines():
+    name = name.strip()
+    if len(name) > 2 or len(name) == 0:
+        continue
+
+    row, col = line/COL_MAX, line%COL_MAX
+
+    if line != ROW_MAX * COL_MAX:
+        line = line + 1
+    else:
+        line = 0
+        fn = fn + 1
+
+        output.close()
+        output = open('whistle%d.scad' % fn, 'w')
+        output.write(SCAD_BEGIN)
+
+    output.write(SCAD_TEMPLATE % (col, row, name))
+
+output.close()
